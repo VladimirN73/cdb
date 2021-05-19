ddfff5edc95a28f3762e93aa7b2d819b9f4d2043
Vladimir Nalivaika <vladimir.nalivaika@bridging-it.de>
2021-05-19 21:45:45 +0200

    ok, dry run is done

--- cdb.Common/Extensions/ConfigurationExtensions.cs
+++ cdb.Common/Extensions/ConfigurationExtensions.cs
@@ -8,7 +8,7 @@ namespace cdb.Common.Extensions
     {
         public static string GetConnectionStringByKey(this IConfiguration config, string strKey)
         {
-            if (strKey.IsNullOrEmpty()) return "";
+            if (strKey.IsNullOrEmpty()) return null;
 
             var list = config
                 .GetSection("ConnectionStrings")
@@ -18,7 +18,7 @@ namespace cdb.Common.Extensions
             var item = list
                 .FirstOrDefault(x => string.Equals(x.Key.Trim(), strKey.Trim(), StringComparison.CurrentCultureIgnoreCase));
 
-            var ret = item?.Value ?? string.Empty;
+            var ret = item?.Value;
 
             return ret;
         }
