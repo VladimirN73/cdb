0c8831e57d126e49b7b357824ed5774dacfe6441
Vladimir Nalivaika <vladimir.nalivaika@bridging-it.de>
2021-06-10 17:10:52 +0200

    add console.unittests

---
+++ tests/cdb.Module.Console.UnitTests/CmdParameterParserTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace cdb.Module.Console.UnitTests
+{
+    [TestFixture]
+    public class CmdParameterParserTest
+    {
+        private CmdParameterParser _sut;
+
+
+
+        public void TryGetParameterValue()
+        {
+            parser.TryGetParameterValue(parameterArray, param_dbSource, out ret.dbSource);
+        }
+    }
+}
